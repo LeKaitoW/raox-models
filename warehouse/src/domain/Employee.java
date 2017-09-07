a5e9ae217e767f7d23fc4e2d07169c8ceb391f82
Mikhail Mineev <mikevbif@yandex.ru>
2017-09-07 18:54:20 +0300

    Добавил наброски взаимодействия с базой (aurusov/raox-models#16)

---
+++ warehouse/src/domain/Employee.java
@@ -0,0 +1,25 @@
+package domain;
+
+import javax.persistence.Entity;
+
+@Entity
+public class Employee {
+    private String firstName;
+    private String lastName;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setFirstName(String fn) {
+        firstName = fn;
+    }
+
+    public void setLastName(String ln) {
+        lastName = ln;
+    }
+}
